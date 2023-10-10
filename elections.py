e43c0fc4c82a0401b102cca5e2e8bacb85c8257e
Dubem <cybazak@gmail.com>
2023-10-10 22:02:46 +0200

    Revert "2nd commit"
    This reverts commit cca49c0e64ebc4ec4d569cae1d0b2433199d605a.

--- elections.py
+++ elections.py
@@ -1,17 +1,16 @@
 # Import a few packages
 import time
 import matplotlib.pyplot as plt
-import functionality as fn
 
 # Initialise the lists to use
 contestants = ['Kwiatowski', 'Lewandowski', 'Zajak', 'Tatomir\n']
 polling_box = []
 
-print("Welcome, everyone, to the Wrocław 2023 Municipal Elections! \nThis is the 69th time this election is being held and today, we have 4 worthy candidates contesting for the position of Head of State!")
+print("Welcome, everyone, to the Wrocław 2023 Municipal Elections! \nThis is the 69th time this election is being held and today, we have 4 worthy candidates contesting for the position of Head of State!\n")
 
 # Loops through the block of code based on a set logical condition
 while True:
-    answer = input('\nWould you like to vote? \n--> ')
+    answer = input('Would you like to vote? \n--> ')
     answer = answer.lower()         # <--- Makes the input to the 'answer' variable case-insensitive
     # 'If/elif' statements to perform a set action depending on the input of 'answer'
     if answer == 'yes':
@@ -20,21 +19,16 @@ while True:
         for c in contestants:
             time.sleep(1)
             print(f'--> {c}')
-        time.sleep(1.5)
         vote = input('Cast your vote: ')
         # Nested if/elif statements to add the results to the 'polling_box' list depending on the candidate voted for
         if vote == '1':
             polling_box.append(contestants[0])
-            print(fn.thanks())
         elif vote == '2':
             polling_box.append(contestants[1])
-            print(fn.thanks())
         elif vote == '3':
             polling_box.append(contestants[2])
-            print(fn.thanks())
         elif vote == '4':
             polling_box.append(contestants[3])
-            print(fn.thanks())
         else:
             print('Invalid input. Not an option.')
     elif answer == 'no':
